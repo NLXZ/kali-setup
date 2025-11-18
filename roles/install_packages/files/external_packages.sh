dc1b0350f00cff950b3e678366f5c5da4227cdee
NLXZ <155401381+NLXZ@users.noreply.github.com>
2025-11-18 20:17:08 +0100

    Update

--- roles/install_packages/files/external_packages.sh
+++ roles/install_packages/files/external_packages.sh
@@ -1,60 +1,78 @@
-#!/bin/sh
+#!/bin/bash
 
-# Python pipx packages
-pipx install impacket pywhisker certipy uro 
-
-pipx install git+https://github.com/login-securite/DonPAPI.git
-
-pipx install git+https://github.com/Pennyw0rth/NetExec
-
-# Python uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
-# Custom pwnc
+# custom
 wget https://raw.githubusercontent.com/NLXZ/pwnc/refs/heads/main/pwnc.sh -O ~/.local/bin/pwnc
 
-# Go packages
-go env -w GOBIN=/home/$USER/.local/bin
+# pipx packages
+packages=(
+    "impacket"
+    "pywhisker"
+    "certipy"
+    "uro"
+    "clairvoyance"
+    "'evil-winrm-py[kerberos]'"
+    "uv"
+    "git+https://github.com/login-securite/DonPAPI.git"
+    "git+https://github.com/Pennyw0rth/NetExec"
+)
 
-go install github.com/0xpugal/dedupe@latest
-go install github.com/tomnomnom/unfurl@latest
-go install github.com/tomnomnom/assetfinder@latest
-go install github.com/tomnomnom/qsreplace@latest
-go install github.com/tomnomnom/gf@latest && mkdir -p ~/.gf && git clone https://github.com/NLXZ/gf-patterns && mv gf-patterns/*.json ~/.gf/ && rm -rf gf-patterns
-go install github.com/lc/gau/v2/cmd/gau@latest
-go install github.com/haccer/subjack@latest
-go install github.com/PentestPad/subzy@latest
-go install github.com/hahwul/dalfox/v2@latest
-go install github.com/Hackmanit/Web-Cache-Vulnerability-Scanner@latest && mv ~/.local/bin/Web-Cache-Vulnerability-Scanner ~/.local/bin/wcvs
-go install github.com/devploit/nomore403@latest
-go install github.com/nlxz/nscope@main
-go install github.com/nlxz/hacks/kxss@latest
-go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest
-go install github.com/projectdiscovery/pdtm/cmd/pdtm@latest && pdtm -bp ~/.local/bin -ia
+for package in "${packages[@]}"; do
+    pipx install "$package"
+done
 
-# Git
-mkdir -p ~/git/
+# go packages
+go env -w GOBIN="$HOME/.local/bin"
 
-git clone https://github.com/urbanadventurer/username-anarchy ~/git/username-anarchy
-ln -s ~/git/username-anarchy/username-anarchy ~/.local/bin/username-anarchy
+urls=(
+    "github.com/0xpugal/dedupe@latest"
+    "github.com/tomnomnom/unfurl@latest"
+    "github.com/tomnomnom/assetfinder@latest"
+    "github.com/tomnomnom/qsreplace@latest"
+    "github.com/tomnomnom/gf@latest"
+    "github.com/lc/gau/v2/cmd/gau@latest"
+    "github.com/haccer/subjack@latest"
+    "github.com/PentestPad/subzy@latest"
+    "github.com/hahwul/dalfox/v2@latest"
+    "github.com/Hackmanit/Web-Cache-Vulnerability-Scanner@latest"
+    "github.com/devploit/nomore403@latest"
+    "github.com/nlxz/nscope@main"
+    "github.com/nlxz/hacks/kxss@latest"
+    "github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest"
+    "github.com/projectdiscovery/pdtm/cmd/pdtm@latest"
+    "github.com/bitquark/shortscan/cmd/shortscan@latest"
+)
 
-git clone https://github.com/s0md3v/Corsy ~/git/Corsy
-chmod +x ~/git/Corsy/corsy.py
-ln -s ~/git/Corsy/corsy.py ~/.local/bin/corsy.py
+for url in "${urls[@]}"; do
+    go install "$url"
+done
 
-git clone https://github.com/defparam/smuggler ~/git/smuggler
-ln -s ~/git/smuggler/smuggler.py ~/.local/bin/smuggler.py
+# gf patterns
+git clone https://github.com/NLXZ/gf-patterns ~/.gf/
 
-git clone https://github.com/coffinxp/loxs ~/git/loxs
-chmod +x ~/git/Corsy/corsy.py
-uv add -r ~/git/loxs/requirements.txt --script ~/git/loxs/loxs.py
-sed -i '1s|.*|#!/usr/bin/env -S uv run --script|' ~/git/loxs/loxs.py
-ln -s ~/git/loxs/loxs.py ~/.local/bin/loxs.py
-axel https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/loxs/chrome.deb
-sudo dpkg -i /tmp/loxs/chrome.deb
-axel https://storage.googleapis.com/chrome-for-testing-public/128.0.6613.119/linux64/chromedriver-linux64.zip -o /tmp/loxs/chromedriver.zip
-unzip /tmp/loxs/chromedriver.zip -d /tmp/loxs/
-sudo mv /tmp/loxs/chromedriver /usr/bin
-rm -rf /tmp/loxs/
+# wcvs
+mv ~/.local/bin/Web-Cache-Vulnerability-Scanner ~/.local/bin/wcvs
 
-curl -s https://codeload.github.com/tomnomnom/hacks/tar.gz/master | tar -xz --strip=2 hacks-master/kxss
+# project discovery tools
+pdtm -bp ~/.local/bin -ia
+
+# docker compose
+mkdir -p ~/.docker/cli-plugins
+cp /usr/bin/docker-compose ~/.docker/cli-plugins/
+
+# bloodhound
+wget https://github.com/SpecterOps/bloodhound-cli/releases/latest/download/bloodhound-cli-linux-amd64.tar.gz
+tar -xvzf bloodhound-cli-linux-amd64.tar.gz -C ~/.local/bin/
+rm -f bloodhound-cli-linux-amd64.tar.gz
+
+# git
+mkdir -p ~/tools/
+
+git clone https://github.com/urbanadventurer/username-anarchy ~/tools/username-anarchy
+ln -s ~/tools/username-anarchy/username-anarchy ~/.local/bin/username-anarchy
+
+git clone https://github.com/s0md3v/Corsy ~/tools/Corsy
+chmod +x ~/tools/Corsy/corsy.py
+ln -s ~/tools/Corsy/corsy.py ~/.local/bin/corsy.py
+
+git clone https://github.com/defparam/smuggler ~/tools/smuggler
+ln -s ~/tools/smuggler/smuggler.py ~/.local/bin/smuggler.py
