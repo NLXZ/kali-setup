5ec30176154f7a312c98563e21cd188926c4c7a3
NLXZ <155401381+NLXZ@users.noreply.github.com>
2025-11-24 14:12:03 +0100

    Update

--- roles/setup_chrome/files/extensions/setup.sh
+++ roles/setup_chrome/files/extensions/setup.sh
@@ -2,8 +2,7 @@
 
 # Start Google Chrome in background
 Xvfb :99 -screen 0 1920x1080x24 &
-timeout 1 sh -c 'DISPLAY=:99 google-chrome --no-sandbox --disable-gpu'
-DISPLAY=:99 google-chrome --no-sandbox --disable-gpu &
+DISPLAY=:99 google-chrome --no-sandbox --disable-gpu --remote-debugging-port=9222 --user-data-dir="$HOME/.config/google-chrome/Default" &
 
 # Wait for the extentensions to be installed
 extensions=$(jq -r 'keys[]' /tmp/extensions/extensions.json | xargs)
@@ -16,12 +15,18 @@ done
 # Ensure all extension config directories
 for extension in $(echo $extensions); do
     url=$(find "$HOME/.config/google-chrome/Default/Extensions/$extension" -name 'options.html' | grep -oP "$extension/.*?/\K.*")
-    echo "chrome-extension://$extension/$url"
+    google-chrome "chrome-extension://$extension/$url"
     while [ ! -d "$HOME/.config/google-chrome/Default/Local Extension Settings/$extension" ]; do
         sleep 1
     done
 done
+
+# Close Google Chrome
+curl -s "http://localhost:9222/json" | jq -r '.[].id' | while read id; do
+    curl -s -X POST "http://localhost:9222/json/close/$id"
+done
 pkill chrome || true
+pkill Xvfb || true
 
 # Configure extensions
 cd /tmp/extensions
